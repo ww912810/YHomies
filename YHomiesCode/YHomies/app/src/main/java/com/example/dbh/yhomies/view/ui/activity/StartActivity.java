c392c9d87986cc959c757a99692bf6ab9f63486b
段博涵 <550550821@qq.com>
2018-05-24 04:05:13 +0800

    新增高清启动图资源 新增颜色资源 修改标题栏样式 新增欢迎页

--- YHomiesCode/YHomies/app/src/main/java/com/example/dbh/yhomies/view/ui/activity/StartActivity.java
+++ YHomiesCode/YHomies/app/src/main/java/com/example/dbh/yhomies/view/ui/activity/StartActivity.java
@@ -1,10 +1,18 @@
 package com.example.dbh.yhomies.view.ui.activity;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.dbh.yhomies.MainActivity;
 import com.example.dbh.yhomies.R;
 import com.example.dbh.yhomies.utils.WindowManagerUtils;
+import com.readystatesoftware.systembartint.SystemBarTintManager;
 
 /**
  * app启动页
@@ -12,10 +20,46 @@ import com.example.dbh.yhomies.utils.WindowManagerUtils;
  */
 public class StartActivity extends AppCompatActivity {
 
+    private Context mContext;
+    private Handler mHandler;
+
+    @SuppressLint("HandlerLeak")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         WindowManagerUtils.SetFullscreenWindow(this);
         setContentView(R.layout.activity_start);
+        //在内容视图设置后创建我们的管理器实例
+        SystemBarTintManager tintManager = new SystemBarTintManager(this);
+        // 设置自定义状态栏颜色
+        tintManager.setTintColor(Color.parseColor("#99ffffff"));
+
+        mHandler = new Handler(){
+            @Override
+            public void handleMessage(Message msg) {
+                switch (msg.what){
+                    case 0:
+                            startActivity(new Intent(mContext,MainActivity.class));
+                            finish();
+                        break;
+                }
+            }
+        };
+        startMainActivity();
     }
+
+    private void startMainActivity(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1000);
+                    mHandler.sendEmptyMessage(0);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
 }
---
+++ YHomiesCode/YHomies/app/src/main/java/com/example/dbh/yhomies/view/ui/activity/WelcomeActivity.java
@@ -0,0 +1,19 @@
+package com.example.dbh.yhomies.view.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.dbh.yhomies.R;
+
+/**
+ * 用户第一次安装进入app欢迎页/引导页
+ * @author 段博涵
+ */
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
